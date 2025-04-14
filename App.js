fe20072fd72b6fef372ddfb40abf3e94d182572f
Egi abdul azis saputra <azissaputraa200@gmail.com>
2025-04-14 23:30:26 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, ScrollView, StatusBar } from 'react-native';
+import styles from './styles/profileStyle';
+import ProfileHeader from './components/ProfileHeader';
+import ProfileDetails from './components/ProfileDetails';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// Data Profil
+const MyBio = {
+  identity: {
+    npm: 212310056,
+    firstname: "MUHAMMAD",
+    middlename: "ZIDAN",
+    lastname: "AKBAR",
   },
-});
+  educations: [
+    { id: 1, school: "SDN 1 Kota Bogor" },
+    { id: 2, school: "SMPN 1 Kota Bogor" },
+    { id: 3, school: "SMAN 1 Kota Bogor" },
+  ],
+  mobile_phone: "0812345679",
+  email: "212310056@student.ibik.ac.id",
+  gender: 'M',
+  tall_body: 175,
+  weight_body: 64.5
+};
+
+export default class App extends Component {
+  render() {
+    const fullName = `${MyBio.identity.firstname} ${MyBio.identity.middlename} ${MyBio.identity.lastname}`;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#2b2d42" />
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <ProfileHeader name={fullName} />
+          <ProfileDetails bio={MyBio} />
+        </ScrollView>
+      </View>
+    );
+  }
+}
